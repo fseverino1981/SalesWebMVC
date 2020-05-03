859410d21bb7679050172f29783b674d79c8c2d2
fseverino1981 <fasvb6@gmail.com>
2020-05-03 09:22:45 -0300

    Asynchronous operations using task (async, await)

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -10,7 +10,7 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
-        [StringLength(6,MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        [StringLength(60,MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
